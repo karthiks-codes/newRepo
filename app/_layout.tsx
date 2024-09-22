a02d4b0444a08521424ca8d0ff5f0a15dcdd458f
Siva Chegondi <siva.chegondi7@gmail.com>
2024-09-22 21:25:28 +0530

    Removing boilerplate code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,9 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 
-export default function Layout() {
-  return <Stack />;
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+    </Stack>
+  );
 }
